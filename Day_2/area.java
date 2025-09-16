e5713f87b6bc6c7ccad2c9f64bca9c7a0b2c6e61
Varshith <varshithram727@gmail.com>
2025-09-16 10:26:07 +0530

    java

---
+++ Day_2/area.java
@@ -0,0 +1,14 @@
+package Day_2;
+
+import java.util.Scanner;
+
+public class area {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int l = sc.nextInt();
+        int b = sc.nextInt();
+        int area = l * b;
+        System.out.println(area);
+
+    }
+}
---
+++ Day_2/even.java
@@ -0,0 +1,16 @@
+package Day_2;
+
+import java.util.Scanner;
+
+public class even {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a % 2==0 ){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
