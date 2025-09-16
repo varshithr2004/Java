445e7d3a40ec57bb9ce7c6bd8f4c3eb6f2dcaef0
Varshith <varshithram727@gmail.com>
2025-09-16 12:28:47 +0530

    16/9/2025

--- Day_3/Largest.java
+++ Day_3/Largest.java
@@ -2,7 +2,7 @@ package Day_3;
 
 import java.util.Scanner;
 
-public class Largest {
+public class Largest{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
