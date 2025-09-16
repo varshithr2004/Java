82f7c73f4e6ba5eb2daf770e7670f08cecaca76f
Varshith <varshithram727@gmail.com>
2025-09-16 12:17:19 +0530

    java

--- Day_3/Leap.java
+++ Day_3/Leap.java
@@ -6,8 +6,16 @@ public class Leap {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if(a % 4 == 0 || a% 100 == 0 || a % 400 ==0){
-            System.out.println("Leap Year");
+        if(a % 4 == 0){
+            if(a % 100 == 0){
+                if(a % 400 ==0){
+                    System.out.println("Leap Year");
+                }else{
+                    System.out.println("Not a Leap Year");
+                }
+            }else{
+                System.out.println("Leap year");
+            }
         }else {
             System.out.println("Not a Leap Year");
         }
