717d87a4871f688692887923c1174da6a67d33a9
Varshith <varshithram727@gmail.com>
2025-09-21 09:32:14 +0530

    21/9/2025

--- Day_5/substring.java
+++ Day_5/substring.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class substring {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str1 = sc.nextLine();
+        String str1 =  sc.nextLine();
         String str2 = sc.nextLine();
         if(str1.equals(str2)){
             System.out.println("Strings are equal");
