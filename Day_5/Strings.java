5426c52fffd3b3a256411db79d9bf2341ec55727
Varshith <varshithram727@gmail.com>
2025-09-21 09:32:29 +0530

    21/9/2025

--- Day_5/Strings.java
+++ Day_5/Strings.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Strings {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         System.out.println(s.toLowerCase());
         System.out.println(s.toUpperCase());
